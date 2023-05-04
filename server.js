4cec423cc2492866d45826de7898e4a0917ad0d0
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-05-04 12:20:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+// Read in the weather from our "Database"
+const data = require('./data/weather.json');
 
 app.get("/", (request, response) => {
     response.send("Hi there");
 });
+
+app.get("/weather", (request, response) => {
+    response.send("Hi there");
+});
+
 
 app.get("/about", (request, response) => {
     response.send("<h1>About</h1>");
