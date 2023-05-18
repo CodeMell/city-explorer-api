2b5e7b86f6cfe118c761b461e1e9689a4b893344
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-05-18 11:54:09 -0500

    up

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require('cors');
 const axios = require('axios');
 // read weather database
-const data = require('./data/weather.json');
+const data = require('./data/weatherAPI.json');
 
 const app = express();
 
@@ -29,15 +29,15 @@ app.get("/", (req, res) => {
 
 
 // get lon, lat, and searchQuery from the form
-app.get("/weather", (req, res) => {
+app.get("/weather", async(req, res) => {
     // let searchQuery = req.query.searchQuery;
-    // let lat = req.query.lat;
-    // let lon = req.query.lon;
-    // let a = {
-    //     lat,
-    //     lon,
-    //     searchQuery
-    // };
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    let a = {
+        lat,
+        lon,
+        // searchQuery
+    };
     // let cityData = data.find(e => {
     //     return(
     //         a.lat === e.lat &&
@@ -50,9 +50,9 @@ app.get("/weather", (req, res) => {
     // }else{
     //     console.log("ERROR: data undefined");
     // }
-    axios.use(process.env.WEATHER_API_KEY)
-
-    let daysOfWeather =  cityData.data.map((day) => {
+    let weatherAPI= await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
+console.log(weatherAPI);
+    let daysOfWeather =  weatherAPI.data.data.map((day) => {
         return new Forecast(day.valid_date, day.weather.description)
     });
     if(!daysOfWeather){
