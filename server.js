427e71c9ee7579d7e01df1e6260b5472bd734f33
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-05-18 15:09:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,10 +23,43 @@ class Forecast {
     }
 }
 
+class Movie{
+    constructor(
+        title,
+        overview,
+        average_votes,
+        total_votes,
+        popularity,
+        released_on
+      ) {
+        this.title = title;
+        this.overview = overview;
+        this.average_votes = average_votes;
+        this.total_votes = total_votes;
+        this.popularity = popularity;
+        this.released_on = released_on;
+      }
+}
+
 app.get("/", (req, res) => {
     res.send("Hi there");
 });
 
+app.get("/movies", async(req, res) => {
+    let searchQuery = req.query.searchQuery;
+    let moviesAPI= await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}&page=1&include_adult=false`);
+    console.log(moviesAPI);
+    let movieOfTheDay =  moviesAPI.data.data.map((day) => {
+        return new Movie(movie.title, movie.overview, movie.vote_average, movie.votes_count, movie.popularity, movie.release_date)
+    });
+    if(!movieOfTheDay){
+        let err = Error("500 Server Error");
+        err.status = 500;
+        next(err);
+    }
+    res.send(movieOfTheDay);
+    
+});
 
 // get lon, lat, and searchQuery from the form
 app.get("/weather", async(req, res) => {
