9e79f084c0157a78ed311d1cdc4e34c9676d640b
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-05-19 14:07:55 -0500

    up

--- movies.js
+++ movies.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const NodeCache = require( "node-cache" );
+const moviesCache = new NodeCache();
 
 class Movie{
     constructor(
@@ -11,14 +13,22 @@ class Movie{
 }
 
 exports.movies = async function (req, res) {
-    let searchQuery = req.query.searchQuery;
-    let moviesAPI= await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}&page=1&include_adult=false`);
-    console.log(moviesAPI);
-    let movieData = moviesAPI.data
-    if(!movieData){
-        let err = Error("500 Server Error");
-        err.status = 500;
-        next(err);
+  let searchQuery = req.query.searchQuery;
+  if(moviesCache.get(searchQuery) != undefined){
+    res.send(moviesCache.get(searchQuery));
+    console.log('data already enterd');
+  }else{
+      let moviesAPI= await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}&page=1&include_adult=false`);
+      console.log(moviesAPI);
+      let movieData = moviesAPI.data
+      if(!movieData){
+          let err = Error("500 Server Error");
+          err.status = 500;
+          next(err);
+      }
+      moviesCache.set(searchQuery,movieData, 3600);
+      res.send(movieData);
+
     }
-    res.send(movieData);
+
 };
--- weather.js
+++ weather.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const NodeCache = require( "node-cache" );
+const weatherCache = new NodeCache();
 
 class Forecast {
     constructor(valid_date, description){
@@ -17,15 +19,21 @@ class Forecast {
         lon
     };
  
-    let weatherAPI= await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
-console.log(weatherAPI);
-    let daysOfWeather =  weatherAPI.data.data.map((day) => {
-        return new Forecast(day.valid_date, day.weather.description)
-    });
-    if(!daysOfWeather){
-        let err = Error("500 Server Error");
-        err.status = 500;
-        next(err);
+    if(weatherCache.get(lon) != undefined){
+        res.send(weatherCache.get(lon));
+        console.log('data already enterd');
+    }else{
+        let weatherAPI= await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`);
+        console.log(weatherAPI);
+        let daysOfWeather =  weatherAPI.data.data.map((day) => {
+            return new Forecast(day.valid_date, day.weather.description)
+        });
+        weatherCache.set(lon,daysOfWeather, 3600);
+        if(!daysOfWeather){
+            let err = Error("500 Server Error");
+            err.status = 500;
+            next(err);
+        }
+        res.send(daysOfWeather);
     }
-    res.send(daysOfWeather);
 };
